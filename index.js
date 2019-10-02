f265864829eba9908dd87d17d31c3cadff7d56e4
tuanna2 <tuanna2@ows.com.vn@gmail.com>
2019-10-02 19:17:50 +0700

    fix some bug

--- index.js
+++ index.js
@@ -2,15 +2,16 @@ const express = require('express');
 const request = require('request');
 const app = express();
 
-app.get('/', (req, res, next) => {
+app.get('/', (req, res) => {
         if(!req.query.link){
             return res.status(500).send("Invalid");
         }
         const proxy = request(req.query.link)
-        .on('response',res => {
-            res.set(res.headers)
+        .on('response',response => {
+            res.set(response.headers)
         })
         .on('error',err => {
+            console.log("err:" ,err);
             res.status(500).send(err);
         })
         proxy.pipe(res);
